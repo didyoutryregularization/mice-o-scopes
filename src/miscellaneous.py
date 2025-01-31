f37094402db7575df6f036552e9f6b2caaa55d81
didyoutryregularization <sam.manson1312@proton.me>
2025-01-31 13:30:43 +0100

    Changed to new config logic and built training function

--- src/miscellaneous.py
+++ src/miscellaneous.py
@@ -1,24 +1,29 @@
 import torch.optim as optim
 import monai.losses as losses
 import torch.nn as nn
-import configparser
-config = configparser.ConfigParser()
-config.read("config.ini")
 
-def get_optimizer_class():
+
+def get_optimizer_class(optimizer_string: str):
     OPTIMIZERS = {
     "adam": optim.Adam,
     "sgd": optim.SGD,
     "rmsprop": optim.RMSprop}
 
-    return OPTIMIZERS[config["Training"]["optimizer"]]
+    if optimizer_string not in OPTIMIZERS:
+        raise ValueError(f"Unknown optimizer: {optimizer_string}. Available: {list(OPTIMIZERS.keys())}")
 
-def get_loss_function():
+
+    return OPTIMIZERS[optimizer_string]
+
+def get_loss_function(loss_function_string: str):
     LOSS_FUNCTIONS = {
         "cross_entropy": nn.BCEWithLogitsLoss,
         "dice": losses.DiceLoss,
         "dice_ce": losses.DiceCELoss
     }
-
-    return LOSS_FUNCTIONS[config["Training"]["loss_function"]]
+    
+    if loss_function_string not in LOSS_FUNCTIONS:
+        raise ValueError(f"Unknown loss function: {loss_function_string}. Available: {list(LOSS_FUNCTIONS.keys())}")
+    
+    return LOSS_FUNCTIONS[loss_function_string]
 
--- src/train_model.py
+++ src/train_model.py
@@ -1,28 +1,69 @@
-import configparser
-from src.u_net import UNet
-from src.training_loop import train_one_epoch, test_one_epoch
-from src.dataloader import MiceHeartDataset
-from src.miscellaneous import get_optimizer_class
+import statistics
+
 import torch
 from torch.utils.data import DataLoader
+from yacs.config import CfgNode
 
-config = configparser.ConfigParser()
-config.read("config.ini")
+from config import get_cfg_defaults
+from src.dataloader import MiceHeartDataset
+from src.miscellaneous import get_loss_function, get_optimizer_class
+from src.training_loop import train_one_epoch, validate_one_epoch
+from src.u_net import UNet
 
 
-def train_model():
-    model = UNet(feature_sizes=config["Model"]["feature_sizes"])
+def train_model(cfg: CfgNode):
+    model = UNet(cfg.TRAINING.feature_sizes)
     model.cuda()
 
-    dataset_train = MiceHeartDataset(image_path=config["Data"]["image_path"], resolution=config["Data"].getint("resolution"))
-    dataloader_train = DataLoader(dataset_train, batch_size=config["Training"].getint("batch_size"), shuffle=True, drop_last=True)
-    optimizer_class = get_optimizer_class()
-    optimizer = optimizer_class(model.parameters(), **dict(config["Training"]["optimizer_hyperparameters"]))
-    scaler = torch.cuda.amp.GradScaler()
-    
-    for i in range(config["Training"].getint("epochs")):
+    dataset_train = MiceHeartDataset(
+        image_path=cfg.DATA.image_path_train, resolution=cfg.DATA.resolution
+    )
+    dataloader_train = DataLoader(
+        dataset_train, batch_size=cfg.TRAINING.batch_size, shuffle=True, drop_last=True
+    )
+
+    dataset_val = MiceHeartDataset(
+        image_path=cfg.DATA.image_path_val, resolution=cfg.DATA.resolution
+    )
+    dataloader_val = DataLoader(dataset_val, batch_size=1)
+
+    optimizer_class = get_optimizer_class(optimizer_string=cfg.TRAINING.optimizer)
+    optimizer = optimizer_class(
+        model.parameters(), **cfg.TRAINING.optimizer_hyperparameters
+    )
+
+    scaler = torch.amp.GradScaler(device="cuda")
+
+    seg_loss = get_loss_function(loss_function_string=cfg.TRAINING.loss_function)
+
+    best_evaluation_metric = torch.inf
+
+    for i in range(cfg.TRAINING.epochs):
         # Train model
-        loss_values = train_one_epoch()
+        loss_values_train = train_one_epoch(
+            model=model,
+            dataloader_train=dataloader_train,
+            optimizer=optimizer,
+            scaler=scaler,
+            seg_loss=seg_loss,
+        )
+        loss_values_val = validate_one_epoch(
+            model=model, dataloader_val=dataloader_val, seg_loss=seg_loss
+        )
+        evaluation_metric = validate_model(model, dataloader_val, seg_loss)  # TODO: implement evaluation metric
+
+        if evaluation_metric < best_evaluation_metric:
+            pass  # TODO: implement saving logic
+
+        print(
+            f"Epoch {i + 1}/{cfg.TRAINING.epochs}: Train loss: {statistics.mean(loss_values_train)}, Validate loss: {statistics.mean(loss_values_val)}, Evaluation metric: {evaluation_metric}"
+        )
+
 
 if __name__ == "__main__":
-    train_model()
+    cfg = get_cfg_defaults()
+    cfg.merge_from_file("experiment.yaml")
+    cfg.freeze()
+    print(cfg)
+
+    train_model(cfg=cfg)
